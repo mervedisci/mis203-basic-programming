829343f1f706b986d8c58adade3edb971a1d1bb9
mervedisci <merve.disci24@gmail.com>
2026-09-20 13:48:56 +0300

    Create student_profile.py

---
+++ week01/student_profile.py
@@ -0,0 +1,8 @@
+#Student Profile
+name = input("Enter your name: ")
+department = input("Enter your department: ")
+age = input("Enter your age: ")
+career_goal = input("Enter your career goal: ")
+
+#printing 
+print(f"---Student Profile--- Name: {name} Department: {department} Age: {age} Career Goal: {career_goal}")
